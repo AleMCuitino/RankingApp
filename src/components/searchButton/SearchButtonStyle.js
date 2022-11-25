a9e68ef040ead0a366e6d9df94990bafa243b942
Albert Arques <albertarques@gmail.com>
2022-11-25 12:13:00 +0100

    Search Button styles

--- src/components/searchButton/SearchButtonStyle.js
+++ src/components/searchButton/SearchButtonStyle.js
@@ -1,9 +1,11 @@
 import styled from "styled-components";
-import { bgDark } from "../../variables/ColorVariables";
+import { bgLight } from "../../variables/ColorVariables";
 
 export const Button = styled.button`
-  ${bgDark};
+  ${bgLight};
   border: none;
   padding: 0.2rem 0.4rem;
   border-radius: 1rem;
+  height: 2rem;
+  cursor: pointer;
 `;
