a09a07fb28d6f53de08076a9fb9537f5331442b0
Albert Arques <albertarques@gmail.com>
2022-11-25 09:06:34 +0100

    Color variables

---
+++ src/variables/ColorVariables.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const dark = "#373737";
+const light = "#fff";
+const primary = "#2370e0";
+const secondary = "#184382";
+
+export const txtDark = `"color: ${dark}`;
+export const txtLight = `color: ${light}`;
+export const txtPrimary = `color: ${primary}`;
+export const txtSecondary = `color: ${secondary}`;
+
+export const bgDark = `background-color: ${dark}`;
+export const bgLight = `background-color: ${light}`;
+export const bgPrimary = `background-color: ${primary}`;
+export const bgSecondary = `background-color: ${secondary}`;
