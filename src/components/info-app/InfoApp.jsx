5cde38b350ce6c3dd54bb2b0602e6c11aa8efbfc
Eberth <itec_19@hotmail.com>
2022-11-29 11:31:32 +0100

    app component modified

--- src/components/info-app/InfoApp.jsx
+++ src/components/info-app/InfoApp.jsx
@@ -3,36 +3,15 @@ import Apps from "../../data/app.json";
 import { App, Container, Icon, TextContainer } from "../card/CardStyle";
 import { Rating } from "@mui/material";
 import { AiFillStar } from 'react-icons/ai'
+import Card from '../card/Card'
 
-function InfoApp() {
-    return (
-      <Container>
-        {Apps &&
-          Apps.map((app) => {
-            return (
-              <App key={app.id}>
-                <Icon src={app.app_icon} alt="descr" />
-                <TextContainer>
-                  <h2>{app.app_name}</h2>
-                  <span>{app.device}</span>
-                  <div className="star">
-                    <AiFillStar />
-                      <span> 
-                        <b>{app.rating}</b>
-                      </span>
-                  </div>
-                  <Rating
-                    name="half-rating-read"
-                    defaultValue={parseInt(app.rating)}
-                    precision={0.1}
-                    readOnly
-                  />
-                </TextContainer>
-              </App>
+function InfoApp({app}) {
+    return (      
+              <>
+                <Card/>
+              </>
               );
-            })}
-      </Container>
-  )
+      
 }
 
 export default InfoApp
--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -2,7 +2,9 @@ import  Card  from '../../components/card/Card'
 import React from 'react'
 import Navbar from '../../components/navbar/Navbar';
 import Footer from '../../components/footer/Footer'
+import Apps from '../../data/app.json'
 import FilterButtonsStyled from '../../components/filter-buttons/FilterButtons';
+import {ContainerApps} from './HomeStyle'
 
 
 export default function Home() {
@@ -10,7 +12,11 @@ export default function Home() {
     <>
       <Navbar/>
       <FilterButtonsStyled/>
-      <Card/>      
+      <ContainerApps>
+      {Apps.map((app) => {
+        return <Card key={app.app_id} app={app}/> 
+      })}
+      </ContainerApps>    
 
     </>
     )
