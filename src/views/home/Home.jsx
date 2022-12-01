c9ebbbdfa7ad401609fd9e218d65e5d854046bd2
Albert Arques <albertarques@gmail.com>
2022-12-01 11:16:30 +0100

    Add secondary filters runing

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -21,41 +21,46 @@ export default function Home() {
   //New array with all "Type" values and add "All" as a "Type" value.
   const allAppTypes = ["All", ...new Set(Apps.map((app) => app.type))];
 
-  const [orderApps, setOrderApps] = useState(bestAppsList);
+  const [orderApps, setOrderApps] = useState(bestAppsList); // Render
   const [appTypes, setDevices] = useState(allAppTypes);
 
-  const [filterApps, setFilterApps] = useState(orderApps);
+  const [currentListOrder, setCurrentListOrder] = useState();
 
   const filterDevice = (type) => {
     if (type === "All") {
-      setOrderApps(orderApps);
+      setOrderApps(currentListOrder);
       return;
     }
-    const filteredData = Apps.filter((app) => app.type === type);
+    const filteredData = currentListOrder.filter((app) => app.type === type);
     setOrderApps(filteredData);
   };
 
   const updateAppsList = (order) => {
     if (order === "Mejores") {
       setOrderApps(bestAppsList);
+      setCurrentListOrder(bestAppsList);
     }
     if (order === "Intermedias") {
-      setOrderApps([
+      const intermediateAppsList = [
         ...new Set(
           Apps.filter((app) => app.rating >= 3 && app.rating < 4).sort((a, b) =>
             a.rating > b.rating ? -1 : 1
           )
         ),
-      ]);
+      ];
+      setOrderApps(intermediateAppsList);
+      setCurrentListOrder(intermediateAppsList);
     }
     if (order === "Peores") {
-      setOrderApps([
+      const worstAppsList = [
         ...new Set(
           Apps.filter((app) => app.rating <= 2).sort((a, b) =>
             parseFloat(a.rating) > parseFloat(b.rating) ? 1 : -1
           )
         ),
-      ]);
+      ];
+      setOrderApps(worstAppsList);
+      setCurrentListOrder(worstAppsList);
     }
   };
 
@@ -64,17 +69,18 @@ export default function Home() {
       <Navbar />
       <ButtonsList devices={allAppTypes} filterDevice={filterDevice} />
       <button onClick={() => updateAppsList("Mejores")}>Mejores</button>
-      <button onClick={() => updateAppsList("Intermedias")} button>
-        Intermedias
-      </button>
+      <button onClick={() => updateAppsList("Intermedias")}>Intermedias</button>
       <button onClick={() => updateAppsList("Peores")}>Peores</button>
       <FilterButtonsStyled />
       {orderApps.map((app) => {
         return (
-          <div key={Math.random(1000000000000000000 * 0.1)}>
-            <h2>{app.app_name}</h2>
-            <h3>{app.rating}</h3>
-          </div>
+          <>
+            <div key={Math.random(1000000000000000000 * 0.1)}>
+              <img src={app.app_icon} alt={app.app_name} />
+              <h2>{app.app_name}</h2>
+              <h3>{app.rating}</h3>
+            </div>
+          </>
         );
       })}
       {/* <ContainerApps apps={apps}>
