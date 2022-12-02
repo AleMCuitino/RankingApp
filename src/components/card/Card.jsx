bab3147e56d113c9e82655b08f1fc04a4380ade0
Eberth <itec_19@hotmail.com>
2022-12-02 12:43:40 +0100

    merging last changes to solve conflicts2

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -8,26 +8,17 @@ import { Link } from "react-router-dom";
 function Card({ app }) {
   return (
     <>
-<<<<<<< HEAD
       <App key={app.id}>
         <Link to={`/${app.id}`}>
           <Icon src={app.app_icon} alt="descr" />
         </Link>
-=======
-      <App>
-        <Icon src={app.app_icon} alt="descr" />
->>>>>>> 9ee13074a19bc158379b8130faf1d57fe15786b4
         <TextContainer>
           <span>
             <b>{app.app_name}</b>
           </span>
           <span>{app.type}</span>
           <div className="star">
-<<<<<<< HEAD
             <AiFillStar style={{ color: "#2670E0" }} />
-=======
-            <AiFillStar />
->>>>>>> 9ee13074a19bc158379b8130faf1d57fe15786b4
             <span>
               <b>{app.rating.toString()}</b>
               {console.log(app.rating.valueOf())}
