f174703a0ebbf3daa6755df411b8a18f082a4ac8
Albert Arques <albertarques@gmail.com>
2022-12-02 09:24:45 +0100

    Card modifications image load stopped

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,33 +2,32 @@ import React from "react";
 import Apps from "../../data/app.json";
 import { App, Container, Icon, TextContainer } from "./CardStyle";
 import { Rating } from "@mui/material";
-import { AiFillStar } from 'react-icons/ai'
+import { AiFillStar } from "react-icons/ai";
 
-function Card({app}) {
+function Card({ app }) {
   return (
     <>
-            <App>
-              <Icon src={app.app_icon} alt="descr" />
-              <TextContainer>
-                <span>
-                  <b>{app.app_name}</b>
-                </span>
-                <span>{app.device}</span>
-                <div className="star">
-                  <AiFillStar/>
-                  <span>
-                    <b>{app.rating}</b>
-                  </span>
-                </div>
-                <Rating
-                  name="half-rating-read"
-                  defaultValue={parseInt(app.rating)}
-                  precision={0.1}
-                  readOnly
-                />
-              </TextContainer>
-            </App>
-         
+      <App>
+        {/* <Icon src={app.app_icon} alt="descr" /> */}
+        <TextContainer>
+          <span>
+            <b>{app.app_name}</b>
+          </span>
+          <span>{app.device}</span>
+          <div className="star">
+            <AiFillStar />
+            <span>
+              <b>{app.rating}</b>
+            </span>
+          </div>
+          <Rating
+            name="half-rating-read"
+            defaultValue={parseFloat(app.rating)}
+            precision={1}
+            readOnly
+          />
+        </TextContainer>
+      </App>
     </>
   );
 }
