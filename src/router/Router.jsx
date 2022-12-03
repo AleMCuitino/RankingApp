e1332841799638263b6411312fc984b88ca1c4de
Albert Arques <albertarques@gmail.com>
2022-12-03 08:54:06 +0100

    Added route gridsystem

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,22 +1,23 @@
 import React from "react";
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import InfoApp from "../components/info-app/InfoApp";
-import Home from "../views/home/Home"
+import Home from "../views/home/Home";
 import SearchResult from "../views/search-result/SearchResult";
 import SingleApp from "../views/single-app/SingleApp";
+import GridSystemTrial from "../views/grid-system-trial/GridSystemTrial";
 
 const Router = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />}></Route>
-                <Route path="/:id" element={<SingleApp />}></Route>
-                <Route path="/searchresult" element={<SearchResult/>}></Route>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/:id" element={<SingleApp />}></Route>
+        <Route path="/searchresult" element={<SearchResult />}></Route>
 
-                
-            </Routes>
-        </BrowserRouter>
-    );
+        <Route path="/gridsystem" element={<GridSystemTrial />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default Router;
