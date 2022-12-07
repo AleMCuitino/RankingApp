2e73c3e142c93a63a47798981587b7b234d3ef6d
Albert Arques <albertarques@gmail.com>
2022-12-07 12:12:12 +0100

    Deleted console.log in Card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -21,7 +21,6 @@ function Card({ app }) {
             <AiFillStar style={{ color: "#2670E0" }} />
             <span>
               <b>{app.rating.toString()}</b>
-              {console.log(app.rating.valueOf())}
             </span>
           </div>
           {/* <Rating
