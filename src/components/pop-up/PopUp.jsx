d84b5af72a04915d173c93ed2288d23d3d9441e9
Albert Arques <albertarques@gmail.com>
2022-12-09 08:27:40 +0100

    Adding PopUp function

--- src/components/pop-up/PopUp.jsx
+++ src/components/pop-up/PopUp.jsx
@@ -11,30 +11,91 @@ import {
   ToggleButtonsType,
 } from "./PopUpStyle";
 
-function PopUp({state, setState}) {
+function PopUp({
+  state,
+  setState,
+  renderAppsList,
+  setRenderAppsList,
+  originalDataOrder,
+}) {
+  const filterAppType = (type) => {
+    const filteredData = originalDataOrder.filter((app) => app.type === type);
+    setRenderAppsList(filteredData); // Estado 1
+  };
+
+  const resetFilterAppType = (type) => {
+    const filteredData = originalDataOrder;
+    setRenderAppsList(filteredData);
+  };
+
+  const reorderFilter = (order) => {
+    if (order === "Best 10") {
+      const filteredData = originalDataOrder
+        .sort((a, b) => (a.rating > b.rating ? -1 : 1))
+        .slice(0, 10);
+      setRenderAppsList(filteredData);
+    } else if (order === "Worst 10") {
+      const filteredData = originalDataOrder
+        .sort((a, b) => (a.rating > b.rating ? 1 : -1))
+        .slice(0, 10);
+      setRenderAppsList(filteredData);
+    }
+  };
+
   return (
     <>
-    {state &&
-    <Overlay>
-      <PopUpContainer>
-        <InputPopUp/>
-        <CloseButton onClick={() => setState(false)}>X</CloseButton>
-        <Span>Filters</Span>
-        <FiltersContainer>
-          <ToggleButtonsTops>
-            <ToggleButton text="10 Top" />
-            <ToggleButton text="10 Worst" />
-          </ToggleButtonsTops>
-          <ToggleButtonsType>
-            <ToggleButton text="Mobile" />
-            <ToggleButton text="Web" />
-            <ToggleButton text="Desktop" />
-          </ToggleButtonsType>
-        </FiltersContainer>
-      </PopUpContainer>
-    </Overlay>
-  }
-  </>
+      {state && (
+        <Overlay>
+          <PopUpContainer>
+            <InputPopUp
+              setRenderAppsList={setRenderAppsList}
+              renderAppsList={renderAppsList}
+              originalDataOrder={originalDataOrder}
+            />
+            <CloseButton onClick={() => setState(false)}>X</CloseButton>
+            <Span>Filters</Span>
+            <FiltersContainer>
+              <ToggleButtonsTops>
+                <ToggleButton
+                  renderAppsList={renderAppsList}
+                  text="Best 10"
+                  name="firstFilter"
+                  filterAppType={reorderFilter}
+                />
+                <ToggleButton
+                  renderAppsList
+                  text="Worst 10"
+                  name="firstFilter"
+                  filterAppType={reorderFilter}
+                />
+              </ToggleButtonsTops>
+              <ToggleButtonsType>
+                <ToggleButton
+                  name="secondFilter"
+                  text="Mobile"
+                  filterAppType={filterAppType}
+                />
+                <ToggleButton
+                  name="secondFilter"
+                  text="Web"
+                  filterAppType={filterAppType}
+                />
+                <ToggleButton
+                  name="secondFilter"
+                  text="Desktop"
+                  filterAppType={filterAppType}
+                />
+                <ToggleButton
+                  name="secondFilter"
+                  text="Reset filter"
+                  filterAppType={resetFilterAppType}
+                />
+              </ToggleButtonsType>
+            </FiltersContainer>
+          </PopUpContainer>
+        </Overlay>
+      )}
+    </>
   );
 }
 
