91d95a3f37dbe5d3d0b075a17c7fb3f3ff8b24db
Albert Arques <albertarques@gmail.com>
2022-12-09 10:57:15 +0100

    Adjust sizes and images

--- src/components/carousel/CarouselStyle.js
+++ src/components/carousel/CarouselStyle.js
@@ -1,11 +1,15 @@
 import styled from "styled-components";
 import { Swiper } from "swiper/react";
+import { bgDark } from "../../variables/ColorVariables";
 
 export const SwiperStyle = styled(Swiper)`
+  ${bgDark};
   position: relative;
   z-index: 0;
+  padding: 1rem;
+  margin: 0.5rem 0;
   .img-carousel {
-    height: 17vh;
+    height: 10rem;
     width: auto;
     margin-left: 2%;
     margin-right: 2%;
