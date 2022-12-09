0e4394ad02fd173bcce3987e70c48fd1a0559d26
Albert Arques <albertarques@gmail.com>
2022-12-09 11:25:26 +0100

    Home add app button to right

--- src/views/home/HomeStyle.js
+++ src/views/home/HomeStyle.js
@@ -1,15 +1,16 @@
 import styled from "styled-components";
 
 export const ContainerApps = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    z-index: -1;
+  display: flex;
+  flex-wrap: wrap;
+  z-index: -1;
 `;
 
 export const IconAddApp = styled.div`
-    font-size: 4vw;
-    position: fixed;
-    bottom:0;
-    right:100;
-    z-index: 1;
+  font-size: 4vw;
+  position: fixed;
+  bottom: 0;
+  right: 1rem;
+  right: 100;
+  z-index: 1;
 `;
