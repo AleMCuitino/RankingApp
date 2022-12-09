c63d76f7858d65340758cc72be1628fbe92cef53
Albert Arques <albertarques@gmail.com>
2022-12-09 08:27:24 +0100

    Refactoizing SearchButton

--- src/components/searchButton/SearchButton.jsx
+++ src/components/searchButton/SearchButton.jsx
@@ -1,16 +1,28 @@
 import React, { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import { Button, Icon } from "./SearchButtonStyle";
-import PopUp from '../pop-up/PopUp';
+import PopUp from "../pop-up/PopUp";
 
-function SearchButton() {
+function SearchButton({
+  renderAppsList,
+  setRenderAppsList,
+  originalDataOrder,
+  setOriginalDataOrder,
+}) {
   const [stateModal1, changeStateModal1] = useState(false);
   return (
     <>
       <Button onClick={() => changeStateModal1(!stateModal1)}>
-        <Icon/>
+        <Icon />
       </Button>
-      <PopUp state={stateModal1} setState={changeStateModal1}/>
+      <PopUp
+        state={stateModal1}
+        setState={changeStateModal1}
+        renderAppsList={renderAppsList}
+        setRenderAppsList={setRenderAppsList}
+        originalDataOrder={originalDataOrder}
+        setOriginalDataOrder={setOriginalDataOrder}
+      />
     </>
   );
 }
